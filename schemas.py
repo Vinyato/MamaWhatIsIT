c072e21e1a61edab1b553d0c09c64e33db58eb1a
Vinyato <92307211+Vinyato@users.noreply.github.com>
2024-04-15 19:41:36 +0300

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,39 @@
+import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class SConstantsAdd(BaseModel):
+    coneHeight: float
+    cylinderHeight: float
+    Quefeed: int
+    Qunderfl: int
+    Flfeed: float
+    psolid: int
+    pfluid: int
+    muliqour: float
+
+
+class SConstants(SConstantsAdd):
+    id: int
+    created: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class SConstantsDict(BaseModel):
+    value: list[SConstants]
+
+
+class SConstantsID(BaseModel):
+    ok: bool = True
+    consts_id: int
+
+
+class SValues(BaseModel):
+    x: float
+    y: float
+
+
+class SResult(BaseModel):
+    value: list[SValues]
